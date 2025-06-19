ef1e278a7c38e7a399cfa16f60163fd7f5c88d7c
krishnasamir <45659882+krishnasamir@users.noreply.github.com>
2025-06-20 01:52:07 +0530

    Update Chat.tsx

--- app/Chat.tsx
+++ app/Chat.tsx
@@ -13,7 +13,8 @@ import { usePostHog } from 'posthog-js/react';
 
 // import { createClient } from '@/utils/supabase/client';
 //import { Reaction } from '@/components/messagebox';
-import type { Reaction } from '@/components/messagebox';
+//import type { Reaction } from '@/components/messagebox';
+import type { Reaction } from '../components/messagebox';
 import { FiMenu } from 'react-icons/fi';
 import Link from 'next/link';
 import { getFreeMessageCount, useFreeTrial } from '@/utils/supabase/actions';
